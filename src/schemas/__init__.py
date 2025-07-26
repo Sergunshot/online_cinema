4132325364cf0409dbd3be0dc41f976d939e13c7
Sergiy Yurov <urovsergej10@gmail.com>
2025-07-26 18:02:21 +0300

    add accounts.py schemas

---
+++ src/schemas/__init__.py
@@ -0,0 +1,15 @@
+from .profiles import ProfileRequestSchema, ProfileResponseSchema
+from .movies import (
+    GenreSchema,
+    DirectorSchema,
+    StarSchema,
+    CertificationSchema,
+    CommentSchema,
+    MovieBaseSchema,
+    MovieDetailSchema,
+    MovieListItemSchema,
+    MovieListResponseSchema,
+    MovieCreateSchema,
+    MovieUpdateSchema,
+)
+from .carts import CartResponse, CartItemResponse
