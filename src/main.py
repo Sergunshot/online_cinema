d1e22950ac316323d52d4b6b3aa6916fbc01827c
Sergiy Yurov <urovsergej10@gmail.com>
2025-07-29 18:21:06 +0300

    add profile_router to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from routes import accounts_router
+from routes import accounts_router, profile_router
+
 
 app = FastAPI(
     title="Online cinema",
@@ -8,3 +9,4 @@ app = FastAPI(
 )
 
 app.include_router(accounts_router, prefix=f"/accounts", tags=["accounts"])
+app.include_router(profile_router, prefix=f"/profile", tags=["profile"])
