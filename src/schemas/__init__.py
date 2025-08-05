69bfc1f28d9502fbf5ea40abf3607d69d93d8e1b
Sergiy Yurov <urovsergej10@gmail.com>
2025-08-05 13:14:21 +0300

    add cart imports to schemas/__init__.py

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -12,17 +12,23 @@ from .accounts import (
     TokenRefreshResponseSchema,
 )
 from .profile import ProfileCreateSchema, ProfileResponseSchema
-# from .movies import (
-#     GenreSchema,
-#     DirectorSchema,
-#     StarSchema,
-#     CertificationSchema,
-#     CommentSchema,
-#     MovieBaseSchema,
-#     MovieDetailSchema,
-#     MovieListItemSchema,
-#     MovieListResponseSchema,
-#     MovieCreateSchema,
-#     MovieUpdateSchema,
-# )
-# from .carts import CartResponse, CartItemResponse
+from .movies import (
+    GenreSchema,
+    DirectorSchema,
+    StarSchema,
+    CertificationSchema,
+    CommentSchema,
+    MovieBaseSchema,
+    MovieDetailSchema,
+    MovieListItemSchema,
+    MovieListResponseSchema,
+    MovieCreateSchema,
+    MovieUpdateSchema,
+)
+from .cart import (
+    MovieInCartSchema,
+    CartItemBaseSchema,
+    CartItemResponseSchema,
+    CartResponseSchema,
+    CartCreateSchema,
+)
